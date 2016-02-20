4b969d3da1e6f237417ea3827d5e6959f02a5cea
Andrew <andrewthart098@gmail.com>
2016-02-20 09:04:07 -0500

    clinton, cruz, trump tweets

--- api/twitter.js
+++ api/twitter.js
@@ -5,20 +5,51 @@ var fs = require('fs');
 var keys = JSON.parse(fs.readFileSync('./keys.json', 'utf8'));
 console.log(keys);
 
-var campaignUrls = {
-    clinton: "https://twitter.com/HillaryClinton"
-};
+var clinton = {
+    url: "https://twitter.com/HillaryClinton",
+    annouce: "April 10, 2015",
+    handle: "@HillaryClinton",
+    annoucementID: "587336319321407488"
+}
 
+var cruz = {
+    url: "https://twitter.com/tedcruz",
+    annouce: "23 Mar 2015",
+    handle: "@tedcruz",
+    annoucementID: "579857596191899648"
+}
 
- 
-// var client = new Twitter({
-//   consumer_key: 'AXuoO0ltYyomCNWKtmefIbiG1',
-//   consumer_secret: 'ca6cn3NYRizju19HOz0PbiMjvg2hKXuLMHO1WeJhmwgNySerHC',
-//   access_token_key: '578152915-ZJdqyBsbHpheU3QIxEP94zqmOzztvjr1dEEC49sO',
-//   access_token_secret: 'oiHyHz4D1eMKmnJUYKJpDsmf4Dc0BXG4B7qtIeGJFbaYR'
+var trump = {
+    url: "https://twitter.com/realdonaldtrump",
+    annouce: "23 Mar 2015",
+    handle: "@realdonaldtrump",
+    annoucementID: "610888547839672321"
+}
+
+var client = new Twitter({
+  consumer_key: keys.consumer_key,
+  consumer_secret: keys.consumer_secret,
+  access_token_key: keys.access_token_key,
+  access_token_secret: keys.access_token_secret
+});
+
+// clinton
+// client.get('search/tweets', {q: clinton.handle, result_type: 'popular', since_id: clinton.annoucementID, count: 100}, function(error, tweets, response){
+//     fs.writeFile('./data/clinton.json', JSON.stringify(tweets, null, 4), function (err) {
+//         if (err) return console.log(err);
+//     });
 // });
-// 
-// 
-// client.get('search/tweets', {q: 'node.js'}, function(error, tweets, response){
-//    console.log(tweets);
+
+// cruz
+// client.get('search/tweets', {q: cruz.handle, result_type: 'popular', since_id: cruz.annoucementID, count: 100}, function(error, tweets, response){
+//     fs.writeFile('./data/cruz.json', JSON.stringify(tweets, null, 4), function (err) {
+//         if (err) return console.log(err);
+//     });
 // });
+
+// trump
+client.get('search/tweets', {q: trump.handle, result_type: 'popular', since_id: trump.annoucementID, count: 100}, function(error, tweets, response){
+    fs.writeFile('./data/trump.json', JSON.stringify(tweets, null, 4), function (err) {
+        if (err) return console.log(err);
+    });
+});
