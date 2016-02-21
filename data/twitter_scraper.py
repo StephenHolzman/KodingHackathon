6b430f8d2bafaf0d762bd6e35d953d3ca1e1567f
Andrew <andrewthart098@gmail.com>
2016-02-21 00:15:46 -0500

    scraper can get it

--- data/twitter_scraper.py
+++ data/twitter_scraper.py
@@ -22,15 +22,19 @@ from_account = driver.find_element_by_name("q")
 start_date = datetime.datetime(2015,3,1,0,0,0)
 end_date = datetime.datetime(2016,2,20,0,0,0)
 
+driver.find_element_by_xpath('//*[@id="page-container"]/div/div[1]/form/fieldset[2]/div[1]/label/input').send_keys("realDonaldTrump")
+driver.find_element_by_xpath('//*[@id="since"]').send_keys(start_date.strftime('%Y-%m-%d'))
+driver.find_element_by_xpath('//*[@id="until"]').send_keys(end_date.strftime('%Y-%m-%d'))
+time.sleep(1)
+driver.find_element_by_xpath('//*[@id="page-container"]/div/div[1]/form/button').click()
 
-driver.find_element_by_xpath('//input[@name="from"]').set_keys("realDonaldTrump")
-driver.find_element_by_xpath('//input[@name="since"]').set_keys(start_date.strftime('%Y-%m%-d%'))
-driver.find_element_by_xpath('//input[@name="until"]').set_keys(end_date.strftime('%Y-%m%-d%'))
+time.sleep(5)
 
-
-
-
-
+elems = driver.find_elements_by_xpath("//*[contains(concat(' ', @class, ' '), ' js-stream-item ')]")
+for elem in elems:
+    with open("./trump/" + strptime(start_date, '%Y-%m-%d') + "__TO__" + strptime(end_date, '%Y-%m-%d'),"a+") as f:
+        f.write(elem.text.encode("utf-8") + "\n##NEW##\n")
+    
 # date_list = datespan(start_date, end_date)
 # 
 # for i,k in zip(date_list[0::2], date_list[1::2]):
